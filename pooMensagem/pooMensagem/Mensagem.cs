e01869e41b36d75df0fa16d19384be0d82631940
Elder Galvão <114260709+3lder7@users.noreply.github.com>
2024-08-21 20:45:47 -0300

    Add files via upload

---
+++ pooMensagem/pooMensagem/Mensagem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    public class Mensagem//classe pública para outros projetos utilizarem ela
+    {                    //sendo privado, apenas o próprio objeto consegue interagir com ele mesmo
+        public String TextoMensagem;//public permitindo a interação com o objeto, dessa forma, não encapsulando;
+        public void ExibirMensagem() {
+            Console.WriteLine(this.TextoMensagem);
+        }
+    }
+}
---
+++ pooMensagem/pooMensagem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)//função Main
+        {
+            Mensagem msg1, msg2;//variável do tipo mensagem (que é a outra classe criada, inicia com maiúsculo obrigatoriamente)
+            msg1 = new Mensagem();//ação (que está na outra classe) executada pelo objeto
+            msg2 = new Mensagem();
+            msg1.TextoMensagem = "Primeiro Objeto";
+            msg1.ExibirMensagem();
+            msg2.TextoMensagem = "Segundo Objeto";
+            msg2.ExibirMensagem();
+            Console.ReadKey();
+        }
+    }
+}
