4b6a5c518475ef4514947894aeaecafac51ec9ec
Elder Galvão <114260709+3lder7@users.noreply.github.com>
2024-08-26 22:11:27 -0300

    Add files via upload

---
+++ POOcalculaIdade/POOcalculaIdade/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOcalculaIdade
+{
+    internal class Pessoa
+    {
+
+        private String nomePessoa;
+        public String propriedadeNome
+        {
+            get { return nomePessoa; }
+            set { nomePessoa = value.ToUpper(); }
+        }
+
+        private int anoNascimento;
+		public int propriedadeNascimento
+		{
+			get { return this.anoNascimento; }
+			set { anoNascimento = value; }
+		}
+
+		private int calcularIdade() 
+		{ 
+			DateTime data = DateTime.Now;//pega data ou ano atual do sistema
+			int ano = data.Year;
+			int idade = ano - this.propriedadeNascimento;
+			return idade;
+		}
+
+		public void ExibirDados() 
+		{
+			Console.WriteLine("Nome: "+this.propriedadeNome);
+			Console.WriteLine("Ano de Nascimento: " + this.propriedadeNascimento);
+			Console.WriteLine("Idade:" + this.calcularIdade());
+		}
+
+	}
+}
---
+++ POOcalculaIdade/POOcalculaIdade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOcalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa objPessoa = new Pessoa();
+
+            Console.WriteLine("Programa");
+
+            Console.WriteLine("Digite seu nome:");
+            objPessoa.propriedadeNome = Console.ReadLine();
+            Console.WriteLine("Digite seu ano de nascimento:");
+            objPessoa.propriedadeNascimento = Convert.ToInt32(Console.ReadLine());
+
+            objPessoa.ExibirDados();
+            Console.ReadKey();//pra dar tempo de ler
+        }
+    }
+}
