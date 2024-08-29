879f42b75623d0f199a7dfa0f60ec6254cfc321f
Elder Galvão <114260709+3lder7@users.noreply.github.com>
2024-08-28 21:13:27 -0300

    Add files via upload

---
+++ ControleDeAnimais/ControleDeAnimais/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAnimais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            Animal animal2 = new Animal();
+            Animal animal3 = new Animal();
+            Animal animal4 = new Animal();
+            Animal animal5 = new Animal();
+
+            int contCachorro = 0;
+            int contGato = 0;
+            int contPeixe = 0;
+
+            Console.WriteLine("Controle de Animais");
+
+            //animal 1
+            Console.Write("Informe o NOME do primeiro animal: ");
+            animal1.acessoNome = Console.ReadLine();
+            Console.Write("Informe o TIPO do primeiro animal (Cachorro, Gato, ou Peixe): ");
+            animal1.acessoTipo = Console.ReadLine();
+            if (animal1.acessoTipo == "Cachorro") {
+                contCachorro++;
+            }
+            if (animal1.acessoTipo == "Gato")
+            {
+                contGato++;
+            }
+            if (animal1.acessoTipo == "Peixe")
+            {
+                contPeixe++;
+            }
+
+            //animal 2
+            Console.Write("Informe o NOME do segundo animal: ");
+            animal2.acessoNome = Console.ReadLine();
+            Console.Write("Informe o TIPO do segundo animal (Cachorro, Gato, ou Peixe): ");
+            animal2.acessoTipo = Console.ReadLine();
+            if (animal2.acessoTipo == "Cachorro")
+            {
+                contCachorro++;
+            }
+            if (animal2.acessoTipo == "Gato")
+            {
+                contGato++;
+            }
+            if (animal2.acessoTipo == "Peixe")
+            {
+                contPeixe++;
+            }
+
+            //animal 3
+            Console.Write("Informe o NOME do terceiro animal: ");
+            animal3.acessoNome = Console.ReadLine();
+            Console.Write("Informe o TIPO do terceiro animal (Cachorro, Gato, ou Peixe): ");
+            animal3.acessoTipo = Console.ReadLine();
+            if (animal3.acessoTipo == "Cachorro")
+            {
+                contCachorro++;
+            }
+            if (animal3.acessoTipo == "Gato")
+            {
+                contGato++;
+            }
+            if (animal3.acessoTipo == "Peixe")
+            {
+                contPeixe++;
+            }
+
+            //animal 4
+            Console.Write("Informe o NOME do quarto animal: ");
+            animal4.acessoNome = Console.ReadLine();
+            Console.Write("Informe o TIPO do quarto animal (Cachorro, Gato, ou Peixe): ");
+            animal4.acessoTipo = Console.ReadLine();
+            if (animal4.acessoTipo == "Cachorro")
+            {
+                contCachorro++;
+            }
+            if (animal4.acessoTipo == "Gato")
+            {
+                contGato++;
+            }
+            if (animal4.acessoTipo == "Peixe")
+            {
+                contPeixe++;
+            }
+
+            //animal 5
+            Console.Write("Informe o NOME do quinto animal: ");
+            animal5.acessoNome = Console.ReadLine();
+            Console.Write("Informe o TIPO do quinto animal (Cachorro, Gato, ou Peixe): ");
+            animal5.acessoTipo = Console.ReadLine();
+            if (animal5.acessoTipo == "Cachorro")
+            {
+                contCachorro++;
+            }
+            if (animal5.acessoTipo == "Gato")
+            {
+                contGato++;
+            }
+            if (animal5.acessoTipo == "Peixe")
+            {
+                contPeixe++;
+            }
+
+            Console.WriteLine("Total de animais");
+            Console.WriteLine("Cachorros: "+contCachorro+" Gatos: "+contGato+ " Peixes: "+contPeixe);
+            Console.ReadKey();//esperar tecla para fechar
+        }
+    }
+}
---
+++ ControleDeAnimais/ControleDeAnimais/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ControleDeAnimais")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ControleDeAnimais")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("382f50c7-e6b3-4c11-80f0-caed2312b54b")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
