41282c10c0fc4b114ca4e9200da0acac1c561774
Elder <eldergalvao07@gmail.com>
2024-09-19 16:56:45 -0300

    att

---
+++ basicoMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace basicoMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ basicoMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace basicoMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
