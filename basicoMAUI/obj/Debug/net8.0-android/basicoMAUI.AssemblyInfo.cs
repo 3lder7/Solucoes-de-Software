851ffd5966337ddeeea6f941d4d970acc673501a
Elder <eldergalvao07@gmail.com>
2024-09-25 21:55:14 -0300

    att2509

--- basicoMAUI/obj/Debug/net8.0-android/basicoMAUI.AssemblyInfo.cs
+++ basicoMAUI/obj/Debug/net8.0-android/basicoMAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basicoMAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+41282c10c0fc4b114ca4e9200da0acac1c561774")]
 [assembly: System.Reflection.AssemblyProductAttribute("basicoMAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basicoMAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
